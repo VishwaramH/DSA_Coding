82ead10265d790feeac8a30b44e9595aadf56d48
vishwaram <55096085+VishwaramH@users.noreply.github.com>
2021-09-06 21:23:34 +0530

    Created rotateArray program

---
+++ rotateArray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int length, rotateBy;
+    scanf("%d",&length);
+    int *array = (int *)(malloc(length * sizeof(int)));
+    int *rotatedArray = (int *)malloc(length * sizeof(int));
+    for(int index = 0; index < length; index++) {
+        scanf("%d",&array[index]);
+    }
+    scanf("%d",&rotateBy);
+    for(int ind = 0; ind < length; ind++) {
+        rotatedArray[ind] = array[(ind+rotateBy)%length];
+        printf("%d ",rotatedArray[ind]);
+    }
+    
+}
